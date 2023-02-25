dd11a366036f8298573b6b11fcf28b5a76d93a62
GulsenZalova <zalova0202@gmail.com>
2023-02-25 05:23:36 +0400

    server folder structure created

--- server/index.js
+++ server/index.js
@@ -1,19 +1,10 @@
 const express=require("express")
 const cors=require("cors")
-const http=require("http")
-const {Server}=require("socket.io")
 const app=express()
 app.use(cors())
-
-const server=http.createServer(app)
-
-const io=new Server(server,{
-    cors:{
-        origin:"http://localhost:3000",
-        methods:["GET","POST"]
-    }
-})
-const PORT=5000
-server.listen(PORT,()=>{
-    console.log("Server is running on port:5000")
+app.use(express.json());
+app.use(express.urlencoded())
+const PORT=process.env.PORT ||5000
+app.listen(PORT,()=>{
+    console.log(`Server is running on port:${PORT}`)
 })
