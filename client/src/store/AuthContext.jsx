1e459d9ef8244b441d68724e6f07084541a17cc4
GulsenZalova <zalova0202@gmail.com>
2023-03-02 22:39:27 +0400

    models,controller and route were created for group chat

--- client/src/store/AuthContext.jsx
+++ client/src/store/AuthContext.jsx
@@ -4,10 +4,20 @@ import { createContext, useState } from "react";
 export const authContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-
-
+    const [user,setUser]=useState(null)
     const [loginStatus, setloginStatus] = useState(false);
-
-    return <authContext.Provider value={{ loginStatus, setloginStatus }}>{children}</authContext.Provider>
+    const [register,setResgister]=useState({
+        username:"",
+        email:"",
+        password:"",
+        image:""
+    })
+    const value={
+        loginStatus,
+        register,
+        setloginStatus,
+        setResgister
+    }
+    return <authContext.Provider value={value}>{children}</authContext.Provider>
 
 }
